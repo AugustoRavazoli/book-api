986997f4281c1d7c0c0f8912c5165202c91e1144
Augusto Ravazoli <augustoravazoli@gmail.com>
2023-05-29 16:38:40 -0300

    initial commit

---
+++ src/test/java/io/github/augustoravazoli/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.augustoravazoli.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
